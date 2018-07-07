83c6681d5c20e7ffb8b5161a1bb54f3a4aa2d09a
pnogal <paulina.nogal@wisc.edu>
2018-07-07 14:22:49 -0500

    Refactor code into modules for readability and reusability. Allow a player to play Minesweeper in the terminal

--- src/board.js
+++ src/board.js
@@ -1,24 +1,3 @@
-class Game {
-	constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-	playMove(rowIndex, columnIndex) {
-		this._board.flipTile(rowIndex, columnIndex);
-
-		if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-			console.log('Game over!');
-			this._board.print();
-		} else if (!this._board.hasSafeTiles()) {
-			console.log('Congratulations! You won the game!');
-			this._board.print();
-    	} else {
-      		console.log('Current Board:');
-      		this._board.print();
-    	}
-	}
-}
-
 class Board {
 	constructor(numberOfRows, numberOfColumns, numberOfBombs) {
 		this._numberOfBombs = numberOfBombs;
@@ -115,5 +94,4 @@ class Board {
 	}
 }
 
-const g = new Game(3, 3, 3);
-g.playMove(0, 0);
+export default Board;
